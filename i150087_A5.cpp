df981ce72b3334eca3d166c034c5d68c1088e70d
Umarahmad87 <umarahmadsiddiqi@outlook.com>
2016-04-14 19:47:26 +0500

    Added files via upload

---
+++ i150087_A5.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : i150087_A5.cpp
+// Author      : Umar Ahmad Siddiqi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
